69f925dd3fb7b6ffeaa7042de64965900c7e799f
daniel <danncode@outlook.com>
2021-10-14 17:48:28 -0500

    Version 0.1.0

---
+++ Password_Generator.py
@@ -0,0 +1,30 @@
+import random
+
+def generator():
+    MAYUS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    MINUS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+
+
+    character = MAYUS + MINUS + NUMS + CHARS
+     
+    random_password= []
+
+    for i in range(15):
+        random_character=random.choice(character)
+        random_password.append(random_character)
+
+    new_password= "".join(random_password)
+    return new_password
+
+
+def run():
+    password= generator()
+    print('Your new password is: '+ password)
+    
+
+
+if __name__ == '__main__':
+    run()
+
